3879c3cb5c51ecfb3d31cd0db93f88ffbb176272
Bartosz Krol <bartosz@iwoca.co.uk>
2015-02-01 23:28:24 +0000

    More Tests

--- app/main/tests/test_basic.py
+++ app/main/tests/test_basic.py
@@ -3,13 +3,19 @@ from unittest2 import TestCase
 from app import create_app
 
 
-class MyTest(TestCase):
+class BasicTestCase(TestCase):
 
     def setUp(self):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
 
+    def tearDown(self):
+        self.app_context.pop()
+
     def test_app_exists(self):
         self.assertFalse(current_app is None)
         self.assertFalse(self.app is None)
+
+    def test_app_is_testing(self):
+        self.assertTrue(current_app.config['TESTING'])
