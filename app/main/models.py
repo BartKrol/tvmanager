a92f10b7ba1994397494a651d5bbc6dc0f05889d
Divus <Divus90@gmail.com>
2015-02-14 12:35:34 +0100

    add series model
    * added model
    * added migration.

--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import relationship, backref
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
@@ -10,6 +11,7 @@ class User(db.Model):
     email = db.Column(db.String(254), unique=True, index=True)
     password_hash = db.Column(db.String(128))
 
+    series = relationship("series_of_user", order_by="series_of_user.id", backref="user")
     def __repr__(self):
         return '<User {0}>'.format(self.email)
 
@@ -24,3 +26,17 @@ class User(db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
+class SeriesOfUser(db.Model):
+    __tablename__ = 'series_of_user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
+    series_id = db.Column(db.Integer, index=True)
+
+    def __repr__(self):
+        return '<series_for_user (0)>'.format(self.user_id, self.series_id)
+
+    @property
+    def series_id(self):
+        return self.series_id
