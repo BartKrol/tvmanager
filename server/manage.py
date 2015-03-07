3c3580438434ba033ef34a7497ddcd5339cef53e
Jakub Z <jakzale@gmail.com>
2015-03-07 14:24:47 +0100

    WIP Cleaning up

--- server/manage.py
+++ server/manage.py
@@ -3,14 +3,20 @@
 
 import os
 
-from flask.ext.script import Manager, Shell
+from flask.ext.script import Manager, Shell, Server
 from flask.ext.migrate import Migrate, MigrateCommand
 
 from app import db, create_app
 
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 manager = Manager(app)
+
+
+def make_shell_context():
+    """Populate stuff in flask shell"""
+    return dict(app=app, db=db)
 
 @manager.command
 def test(coverage=False):
@@ -26,14 +32,11 @@ def test(coverage=False):
         arguments.append('--with-coverage')
         arguments.append('--cover-package=app')
 
-    nose.main(argv=arguments)
+    return nose.main(argv=arguments)
 
 
-def make_shell_context():
-    return dict(app=app, db=db)
-
-
-manager.add_command("shell", Shell(make_context=make_shell_context, use_bpython=True))
+# Populate commands
+manager.add_command('shell', Shell(make_context=make_shell_context, use_bpython=True))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
